65e334eab5889e2ef7e42c913c3de4988fec06e8
FontaneBeveriniNapoli <fontane.beverini.napoli@gmail.com>
2025-12-07 23:12:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'fontane-beverini-v2.0.2';
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const CACHE_NAME = 'fontane-beverini-v2.0.3';
+const STATIC_CACHE = 'static-v2.0.3';
+const DYNAMIC_CACHE = 'dynamic-v2.0.3';
 
 const STATIC_ASSETS = [
   './',
