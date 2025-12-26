09c68556963a3c3b0d397e6957fc5b6f16a857b6
FontaneBeveriniNapoli <fontane.beverini.napoli@gmail.com>
2025-12-26 11:08:52 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fontane-beverini-v3.6.8'; // VERSIONE AGGIORNATA
+const CACHE_NAME = 'fontane-beverini-v3.6.9'; // VERSIONE AGGIORNATA
 const STATIC_CACHE = 'static-v3';
 const DYNAMIC_CACHE = 'dynamic-v2';
 
