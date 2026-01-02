9f87d87b758700caca4e52e78e79e9d3945e0ddf
FontaneBeveriniNapoli <fontane.beverini.napoli@gmail.com>
2026-01-02 14:32:29 +0100

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -4,17 +4,26 @@
 if (!window.firebaseInitialized) {
     console.log('Initializing Firebase...');
     
-    // Firebase configuration (same as in index.html)
+    // Nuova configurazione EUROPA (COMPLETA)
     const firebaseConfig = {
-        apiKey: "AIzaSyBvDfxm-LSAcu0NwtJ8DYxxrjY-83LlLPU",
-        authDomain: "abcnapolifontane.firebaseapp.com",
-        projectId: "abcnapolifontane",
-        storageBucket: "abcnapolifontane.firebasestorage.app",
-        messagingSenderId: "686936372148",
-        appId: "1:686936372148:web:4147bab1bab73583b638e1",
-        measurementId: "G-DPEC2SNGDM"
+        apiKey: "AIzaSyCc7latF6oH6gUNE02gAqwlWNsjVeC5gI4",
+        authDomain: "abcnapolifontanebeverini.firebaseapp.com",
+        projectId: "abcnapolifontanebeverini",
+        storageBucket: "abcnapolifontanebeverini.firebasestorage.app",
+        messagingSenderId: "212459088024",
+        appId: "1:212459088024:web:57e11ef44c10d307cd88ed",
+        measurementId: "G-43PW58S5TL"
     };
     
+    // Initialize Firebase (Standard method if library is loaded via script tag)
+    if (typeof firebase !== 'undefined') {
+        firebase.initializeApp(firebaseConfig);
+        // Tenta di inizializzare analytics se la libreria è presente
+        if (firebase.analytics) {
+            firebase.analytics();
+        }
+    }
+
     // Set flag to prevent double initialization
     window.firebaseInitialized = true;
     
